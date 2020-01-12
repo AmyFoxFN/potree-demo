43113f98f78d081c13aba27efc0bf3c515dca4d4
AmyFoxFN <funanamy@gmail.com>
2020-01-12 10:44:08 +0800

    feat: import potree

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three'
+import jQuery from 'jquery'
+import proj4 from 'proj4'
+
+window.THREE = THREE
+window.jQuery = jQuery
+window.proj4 = proj4
+
+const Potree = import('@3dr/potree')
+
+// const cloadContainer = document.getElementById('potree-point-cloud')
+// const viewer = new Potree.Viewer(document.getElementById('potree_render_area'))
